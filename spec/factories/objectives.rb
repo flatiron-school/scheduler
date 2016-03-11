d5921e93da7e1a8787b066bcba73c4e12cacf48c
joshuabamboo <joshuaowens011@gmail.com>
2016-03-11 15:22:02 -0500

    write model tests for an objective's attributes and associations

--- spec/factories/objectives.rb
+++ spec/factories/objectives.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :objective do
     schedule nil
-    content "MyString"
+    content "create a nested form"
   end
 end
--- spec/models/objective_spec.rb
+++ spec/models/objective_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Objective, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Objective" do
+  before(:each) do
+    s = FactoryGirl.build(:schedule)
+    @objective = FactoryGirl.build(:objective)
+    @objective.schedule = s
+  end
+
+  describe "attributes" do
+    it "has content" do
+      expect(@objective.content).to eq("create a nested form")
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a schedule" do
+      expect(@objective.schedule).to be_an_instance_of(Schedule)
+    end
+  end
 end
