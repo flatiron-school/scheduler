991739efebba78fd24761b0a188fdff4ebeb5854
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-03-19 09:27:56 -0400

    styled non-active cohort warning, fixed ajax bug around click of 'add cohort' button

--- app/controllers/user_cohorts_controller.rb
+++ app/controllers/user_cohorts_controller.rb
@@ -2,21 +2,14 @@ class UserCohortsController < ApplicationController
 
 
   def create
-    @user_cohort = UserCohort.create(user_id: current_user.id, cohort_id: params[:cohort_id])
+    @former_active = current_user.active_cohort
+    @cohort = Cohort.find_by_name(params["cohort_id"])
+    @user_cohort = UserCohort.create(user_id: current_user.id, cohort_id: @cohort.id)
     @cohort = @user_cohort.cohort
     respond_to do |format|
       format.js
     end
   end
-
-  # def destroy
-  #   @cohort = Cohort.find_by_name(params["id"])
-  #   uc = UserCohort.find_by(user: current_user, cohort: @cohort)
-  #   uc.destroy
-  #   respond_to do |format|
-  #     format.js {render 'create.js.erb'}
-  #   end
-  # end
 
   def update
     @former_active = current_user.active_cohort
