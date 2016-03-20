a442431d6a59e7f080a400ad03f63603bb42e944
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-03-20 09:30:12 -0400

    working calendar id explanation popover

--- app/assets/javascripts/cohorts.js
+++ app/assets/javascripts/cohorts.js
@@ -1,18 +1,3 @@
-// $(function () {
-  // $('.popover-test').popover('hide');
-  // $('.popover-test').popover({ 
-  //   html : true,
-  //   trigger: "hover"
-  // }).popover('show');
-
-// });
-
-// $(function () {
-//   $('[data-toggle="popover"]').popover()
-// })
-
 $(function () {
-  // $('[data-toggle="tooltip"]').tooltip()
-  // $('[data-toggle="popover"]').popover()
   $('[data-toggle="popover"]').popover({trigger: 'hover','placement': 'top'});
 })
