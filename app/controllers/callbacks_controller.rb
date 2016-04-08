4995d8f65d9cc1f3dd52968e5c1207c32b747097
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-08 10:10:21 -0400

    change instance variable to local variable in google callback method

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,13 +1,9 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  # def github
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-  #   sign_in_and_redirect @user
-  # end
 
   def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
-    if @user
-      sign_in @user
+    user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+    if user
+      sign_in user
       redirect_to root_path
     else
       redirect_to new_user_session_path, notice: 'Access Denied. Please make sure you are signing in with your Flatiron gmail account.'
