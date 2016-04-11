95121e86c66f460afc8f6eb39cf45997f48af891
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-04-11 13:29:11 -0400

    fix bug with add lab/objectve/activity field on schedule form hbs templates for new fields didn't give data attributes to new fields

--- app/assets/javascripts/schedule_form.js
+++ app/assets/javascripts/schedule_form.js
@@ -13,7 +13,7 @@ $.fn.addMore = function(e) {
     if ($('[data-' + addType + '-position]').length >= 1) {
       var newIdNum = $('[data-' + addType + '-position]').last().data()[addType + "Position"] + 1;
     }
-    $("#add-" + addType + "s").append(HandlebarsTemplates['add-' + addType]({newIdNum: newIdNum}));
+    $("#add-" + addType + "s").append(HandlebarsTemplates['add-' + addType]({newIdNum: newIdNum, addType: addType}));
   })
 };
  
