00eb9c27be0a1e3a9a2704b820d1de18395a55bb
antoinfive <antoinfive@gmail.com>
2016-04-12 13:07:35 -0400

    add video link partial and form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,17 @@
 class VideosController < ApplicationController
+
+  def create
+    binding.pry
+  end
+
+  def new
+
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:link, :cohort_id)
+  end
+
 end
