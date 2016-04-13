3f76b9edbab30831ec85caa83ab2cf63bace6226
antoinfive <antoinfive@gmail.com>
2016-04-13 14:49:55 -0400

    add more test and validations

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :video do
     title "Intro to Git"
     link "https://www.youtube.com/watch?v=bK7i-BMJcM0&feature=youtu.be"
-    association :cohort, factory: :cohort
   end
 end
