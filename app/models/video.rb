2dcd1dbc48605d6955d274c1e56a66f8efd137ff
antoinfive <antoinfive@gmail.com>
2016-04-13 14:23:51 -0400

    add video test and validations

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   belongs_to :cohort
+
+  validates :title, presence: true
+  validates :link, uniqueness: true, presence: true, format: {with: /https:\/\/youtu.be\/|https:\/\/www.youtube.com/, message: "must be a valid youtube link"}
+
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Video, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#title" do
+    it "has a title" do
+      video = FactoryGirl.build(:video)
+      expect(video.title).to eq("Intro to Git")
+    end
+  end
+
+  describe "#link" do
+    it "is valid with a youtube link" do
+      video = FactoryGirl.build(:video)
+      expect(video).to  be_valid
+    end
+
+    it "is valid with a sharable youtube link" do
+      video = FactoryGirl.build(:video, link: "https://youtu.be/bK7i-BMJcM0")
+      expect(video).to be_valid
+    end
+
+    it "is invalid without a youtube link" do
+      video = FactoryGirl.build(:video, link: "https://reddit.com")
+      expect(video).to be_invalid
+    end
+  end
 end
