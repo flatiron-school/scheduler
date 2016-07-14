63a744daa8310ed62f2bbc0af567b0f5fd989854
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-07-14 16:45:25 -0400

    schedule deployment service, cron job for daily schedule deployments

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -3,19 +3,13 @@ class DeploymentsController < ApplicationController
   before_action :set_schedule
 
   def create
-    @schedule.update(deploy: true)
-    @schedule.deploy_to_readme(GithubWrapper.new, render_schedule_markdown)
+   ScheduleDeploymentHandler.new(@schedule).execute
     respond_to do |format|
       format.js {render template: 'cohorts/schedules/deploy.js.erb'}
     end
   end
 
   private
-
-    def render_schedule_markdown
-      html_string = ScheduleTemplater.generate_template(@schedule)
-      MarkdownConverter.convert(html_string)
-    end
 
     def set_schedule
       @schedule = Schedule.find(params[:schedule_id])
